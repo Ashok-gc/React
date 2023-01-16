181f7f42b901d57942077098a326a76250768800
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2023-01-16 08:35:46 +0545

    search forms

--- src/App.js
+++ src/App.js
@@ -1,50 +1,86 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+
 
 function App(props) {
+  const [newNote,setNewNote] = useState(' ')
+  const [notes,setnotes] = useState(props.notes)
+ const [showAll ,setshowall] =  useState(true)
 
-  // const {notes}= props.notes
-  const [newNote, setNewNote]=useState('')
-  const [notes, setNotes]= useState([])
+
+ const notestoshow =  showAll ? notes
+ : notes.filter(n => n.important === true)
 
   useEffect(()=>{
-    axios.get('http://localhost:3001/notes')
-    .then((response)=>{
+    axios.get("http://localhost:3001/notes").then((response)=>{
       console.log(response)
-      setNotes(response.data)
+      setnotes(response.data)
     })
+},[])
 
-  },[])
-
-  const handleInputChange = (event) => {
+  const handleInputChange = (event)=>{
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
-  const handleAdd = (event)=>{
+  const HandleAdd = (event) =>{
     event.preventDefault()
-    // alert('Testing...')
+    // create notes
+
+    const note = {
+      id : notes.lenght +1,
+      content : newNote,
+      date : new Date().toString(),
+      important : Math.random() < 0.5
+
+
+
+    }
+
+
+    if(newNote !== "")setnotes(notes.concat(note))
+    setNewNote("")
+
   }
+
+  const setdelete =  (id) =>{
+    var response = window.confirm(`do you really want to delete note with id ${id}`)
+    if(response === true){
+      setnotes(notes.filter(n => n.id !== id))
+    }
+    else{
+      setnotes(notes)
+      
+    }
+    
+
+  }
+const tooglebutton = showAll ? "show impotant" : "show all"
+
+
+
 
   return (
     <>
+   
       <h2>Notes</h2>
       <ul>
-        {notes.map(note =>
-        <li key ={note.id}>
-          {note.content}
-        </li>)}
+        {notestoshow.map(note => <li key = {note.id}><p>{note.content} <button onClick={()=>setdelete(note.id)}>delete</button></p> <p>{note.date}</p></li>)}
+        
       </ul>
+      <button onClick={()=> setshowall(!showAll)}>{tooglebutton} </button>
       <form>
-        <input value={newNote} onChange={handleInputChange}/>
-        <button onClick={handleAdd}>add</button>
+        <input value = {newNote} onChange = {handleInputChange}/> 
+        <button onClick={HandleAdd}> add </button>
       </form>
+      
     </>
-
+    
 
   );
-
-
 }
 
 export default App;
